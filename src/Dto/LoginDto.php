ead21abedf5ba5bbd58586a915f891e30af27ad1
matiw219 <mateusz.wielechowski1@gmail.com>
2023-09-08 13:11:23 +0200

    Add LoginDto

---
+++ src/Dto/LoginDto.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Dto;
+
+class LoginDto
+{
+
+    // username or Email
+    private ?string $user;
+
+    private ?string $password;
+
+    /**
+     * @return string|null
+     */
+    public function getUser(): ?string
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param string|null $user
+     */
+    public function setUser(?string $user): void
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string|null $password
+     */
+    public function setPassword(?string $password): void
+    {
+        $this->password = $password;
+    }
+
+}
