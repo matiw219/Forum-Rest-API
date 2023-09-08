df63b488615e36813fb75bb584a60df5db79e52e
matiw219 <mateusz.wielechowski1@gmail.com>
2023-09-08 13:14:39 +0200

    Controller for testing logging

---
+++ src/Controller/TestLoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\AuthTokenService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * ONLY FOR CHECK
+ */
+class TestLoginController extends AbstractController
+{
+
+    public function __construct(
+        private AuthTokenService $tokenService
+    ) {
+    }
+
+    #[Route('/check/{token}', name: 'app_check', methods: ['GET'])]
+    public function index(?string $token): JsonResponse
+    {
+        $user = $this->tokenService->loggedInAs($token);
+
+        if (!$user) {
+            return new JsonResponse(['You`re not logged in'], 404);
+        }
+        return new JsonResponse(['You`re logged in']);
+    }
+}
