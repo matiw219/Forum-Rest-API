868ea1236bfee3aba4e5956e6974be465b0228eb
matiw219 <mateusz.wielechowski1@gmail.com>
2023-09-13 22:26:48 +0200

    PostDto

---
+++ src/Dto/PostDto.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Dto;
+
+class PostDto
+{
+    public function __construct(
+        private readonly string $title,
+        private readonly string $content
+    ) {
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+}
