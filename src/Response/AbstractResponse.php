17d2bd23ce226ec5e81bdd7b4f758f3220daf5c9
matiw219 <mateusz.wielechowski1@gmail.com>
2023-09-14 20:26:40 +0200

    Abstract Response

---
+++ src/Response/AbstractResponse.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Response;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+abstract class AbstractResponse
+{
+    public function __construct(
+        private readonly int $code = 200
+    ) {
+    }
+
+    public function getCode(): int
+    {
+        return $this->code;
+    }
+
+    abstract public function toJson(): JsonResponse;
+}
---
+++ src/Response/ErrorResponse.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Response;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ErrorResponse extends AbstractResponse
+{
+    public function __construct(
+        private readonly string $error,
+        int $code = 400
+    ) {
+        parent::__construct($code);
+    }
+
+    public function getError(): string
+    {
+        return $this->error;
+    }
+
+    public function toJson(): JsonResponse
+    {
+        return new JsonResponse([
+            'error' => [
+                'code' => $this->getCode(),
+                'message' => $this->getError()
+            ]
+        ]);
+    }
+}
