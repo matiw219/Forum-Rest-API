c4445369204300cc65013f6e61d448196e6a53cf
matiw219 <mateusz.wielechowski1@gmail.com>
2023-09-14 15:21:32 +0200

    Functions to format comments

--- src/Service/CommentService.php
+++ src/Service/CommentService.php
@@ -12,6 +12,28 @@ class CommentService
     ) {
     }
 
+    public function formatComment(Comment $comment): array
+    {
+        return [
+            'id' => $comment->getId(),
+            'content' => $comment->getContent(),
+            'post' => $comment->getPost()->getId(),
+            'createdBy' => ($comment->getCreatedBy()?->getId()),
+            'createdAt' => $comment->getCreatedAt()
+        ];
+    }
+
+    public function formatComments(array $comments): array
+    {
+        $formattedComments = [];
+
+        foreach ($comments as $comment) {
+            $formattedComments[] = $this->formatComment($comment);
+        }
+
+        return $formattedComments;
+    }
+
     public function findById(int $id): ?Comment
     {
         return $this->commentRepository->find($id);
