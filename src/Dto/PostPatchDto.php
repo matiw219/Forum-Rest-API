a1e58308deb969143f37611077ce007cc8ffab52
matiw219 <mateusz.wielechowski1@gmail.com>
2023-09-14 14:55:31 +0200

    Add PostPatchDto

---
+++ src/Dto/PostPatchDto.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Dto;
+
+class PostPatchDto
+{
+    public function __construct(
+        private readonly ?int $createdBy,
+        private readonly ?int $category,
+        private readonly ?string $title,
+        private readonly ?string $content,
+        private readonly ?int $views
+    ) {
+    }
+
+    public function getCreatedBy(): ?int
+    {
+        return $this->createdBy;
+    }
+
+    public function getCategory(): ?int
+    {
+        return $this->category;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function getViews(): ?int
+    {
+        return $this->views;
+    }
+
+}
