b5c03cb4b48fa32737cee9d9a17645bc60f59530
matiw219 <mateusz.wielechowski1@gmail.com>
2023-09-14 15:26:52 +0200

    Get All Comments Service

--- src/Service/CommentService.php
+++ src/Service/CommentService.php
@@ -3,13 +3,73 @@
 namespace App\Service;
 
 use App\Entity\Comment;
+use App\Paginator\Paginator;
 use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class CommentService
 {
     public function __construct(
-        private readonly CommentRepository $commentRepository
+        private readonly CommentRepository $commentRepository,
+        private readonly Paginator $paginator
     ) {
+    }
+
+    public function getAll(Request $request): JsonResponse
+    {
+        $page = $request->get('page');
+
+        if ($page == null) {
+            $comments = $this->getAllComments();
+            $commentsCount = count($comments);
+
+            return new JsonResponse([
+                'info' => [
+                    'page' => -1,
+                    'maxResults' => $commentsCount,
+                    'results' => $commentsCount
+                ],
+                'comments' => $this->formatComments($comments)
+            ], 200);
+        }
+
+
+        $maxResults = (int) $request->get('maxResults', Paginator::DEFAULT_MAX_RESULTS);
+        $comments = $this->getComments($page, $maxResults);
+
+        if (0 === count($comments)) {
+            return new JsonResponse([
+                'error' => 'Page not found'
+            ], 404);
+        }
+
+        return new JsonResponse([
+            'info' => [
+                'page' => (int) $page,
+                'maxResults' => $maxResults,
+                'results' => count($comments)
+            ],
+            'comments' => $this->formatComments($comments)
+        ], 200);
+    }
+
+    private function getComments(int $page = 1, int $maxResults = Paginator::DEFAULT_MAX_RESULTS): array
+    {
+        if ($page == (-1)) {
+            return $this->getAllComments();
+        }
+
+        $this->paginator->setEntity(Comment::class);
+        $this->paginator->setPage($page);
+        $this->paginator->setMaxResults($maxResults);
+
+        return $this->paginator->getResults();
+    }
+
+    public function getAllComments(): ?array
+    {
+        return $this->commentRepository->findAll();
     }
 
     public function formatComment(Comment $comment): array
